46d4b74239bd8b75720fa2b6a0459cc8d7a41165
Yogaz101 <jabari_charles75@outlook.com>
2018-10-22 17:00:10 -0500

    fourth commit

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -2,6 +2,8 @@ window.onload = function () {
     var border = document.getElementById('boundary1');
     var all= document.querySelectorAll('.boundary');
     var thig= document.getElementsByClassName('.boundary');
+    var finish= document.getElementById('end');
+    var start= document.getElementById('start');
     
     function red(obj){
         obj.addEventListener("mouseover", function(){ obj.className += ' youlose'});
@@ -13,6 +15,27 @@ window.onload = function () {
         });
     }
     
+    function youWin(){
+        var check=false;
+        all.forEach(function (y){
+            if (y.classList.contains("youlose")){
+                console.log("it is true");
+            }
+            else{
+                finish.addEventListener("mouseover", function(){ alert("You win!")});
+            }
+        });
+    }
+    
+    function restart(){
+            start.addEventListener("click", function(){ all.forEach(function (y){
+                y.className = 'boundary'
+            })});
+    }
+    
+    
     red(border);
     changeAll();
+    /*youWin();*/
+    restart();
 }
